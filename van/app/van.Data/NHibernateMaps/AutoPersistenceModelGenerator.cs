e96d307ce225b7ee7c4c2c9c8f8c4f3e1f75c569
zpyoung <zpyoung@50af15be-2897-11de-ae70-5144d73449a4>
2009-04-14 04:06:39 +0000

    Intitial checkin

---
+++ van/app/van.Data/NHibernateMaps/AutoPersistenceModelGenerator.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Linq;
+using FluentNHibernate;
+using FluentNHibernate.AutoMap;
+using SharpArch.Core.DomainModel;
+using SharpArch.Data.NHibernate.FluentNHibernate;
+using van.Core;
+
+namespace van.Data.NHibernateMaps
+{
+    public class AutoPersistenceModelGenerator : IAutoPersistenceModelGenerator
+    {
+        public AutoPersistenceModel Generate()
+        {
+            AutoPersistenceModel mappings = AutoPersistenceModel
+                // If you delete the default class, simply point the following line to an entity within the .Core layer
+                .MapEntitiesFromAssemblyOf<Class1>()
+                .Where(GetAutoMappingFilter)
+                .WithConvention(GetConventions)
+                .UseOverridesFromAssemblyOf<AutoPersistenceModelGenerator>();
+
+            return mappings;
+        }
+
+        /// <summary>
+        /// Provides a filter for only including types which inherit from the IEntityWithTypedId interface.
+        /// </summary>
+        private bool GetAutoMappingFilter(Type t)
+        {
+            return t.GetInterfaces().Any(x =>
+                 x.IsGenericType && x.GetGenericTypeDefinition() == typeof(IEntityWithTypedId<>));
+        }
+
+        /// <summary>
+        /// This project's conventions (which may be changed) are as follows:
+        /// * Table names are plural
+        /// * The Id of an object is "Id"
+        /// * Foreign keys are "ObjectNameFk"
+        /// * One-to-Many relationships cascade "All"
+        /// 
+        /// Feel free to change this to your project's needs!
+        /// </summary>
+        private void GetConventions(Conventions c)
+        {
+            c.GetTableName = type => Inflector.Net.Inflector.Pluralize(type.Name);
+            c.IsBaseType = IsBaseTypeConvention;
+            c.FindIdentity = type => type.Name == "Id";
+            c.GetForeignKeyName = type => type.Name + "Fk";
+            c.GetForeignKeyNameOfParent = type => type.Name + "Fk";
+            c.OneToManyConvention = o => o.Cascade.All();
+        }
+
+        private bool IsBaseTypeConvention(Type arg)
+        {
+            bool derivesFromEntity = arg == typeof(Entity);
+            bool derivesFromEntityWithTypedId = arg.IsGenericType &&
+                (arg.GetGenericTypeDefinition() == typeof(EntityWithTypedId<>));
+
+            return derivesFromEntity || derivesFromEntityWithTypedId;
+        }
+    }
+}
---
+++ van/app/van.Web/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("van.Web")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyProduct("van.Web")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c3b8ecb7-3441-42b2-8f71-d7fa4342a19c")]
