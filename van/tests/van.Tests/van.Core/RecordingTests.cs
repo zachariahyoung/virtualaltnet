9abc37a337ba5274421570b1ec8001e148d7fabc
zpyoung <zpyoung@50af15be-2897-11de-ae70-5144d73449a4>
2009-04-18 20:58:44 +0000

    Test for Recording

---
+++ van/tests/van.Tests/van.Core/RecordingTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+using van.Core;
+namespace Tests.van.Core
+{   
+    [TestFixture]
+    public class RecordingTests
+    {
+        [Test]
+        public void CanCreateRecording()
+        {
+            string title = "Unit Testing";
+            string url = "http://www.zachariahyoung.com";
+
+            Recording recording = new Recording();
+            recording.RecordingTitle = title;
+            recording.RecordingUrl = url;
+
+            Assert.That(recording.RecordingTitle, Is.EqualTo(title));
+
+        }
+
+        [Ignore]
+        [Test]
+        public void CanCompareRecording()
+        {
+            Recording instance = new Recording();
+            instance.RecordingTitle = "Joe";
+            instance.RecordingUrl = "http://www.cnn.com";
+
+            Recording instanceToCompareTo = new Recording();
+            instanceToCompareTo.RecordingTitle = "Joe";
+            instanceToCompareTo.RecordingUrl = "http://www.cnn.com";
+
+            Assert.That(instance.Equals(instanceToCompareTo));
+        }
+
+    }
+}
+
