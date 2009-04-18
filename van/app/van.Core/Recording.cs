74c15020991f2a9d424f4b8047d505033609a47b
zpyoung <zpyoung@50af15be-2897-11de-ae70-5144d73449a4>
2009-04-18 20:59:18 +0000

    Recording object

---
+++ van/app/van.Core/Recording.cs
@@ -0,0 +1,9 @@
+namespace van.Core
+{
+    public class Recording
+    {
+        public Recording(){}
+        public string RecordingTitle { get; set; }
+        public string RecordingUrl { get; set; }
+    }
+}
