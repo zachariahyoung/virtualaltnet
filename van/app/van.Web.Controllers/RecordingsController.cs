251e370e1a29273acebf0c85dda0bbb5a9576289
zpyoung <zpyoung@50af15be-2897-11de-ae70-5144d73449a4>
2009-04-19 02:20:24 +0000

    Testing is passing for RecordingsController

--- van/app/van.Web.Controllers/RecordingsController.cs
+++ van/app/van.Web.Controllers/RecordingsController.cs
@@ -1,17 +1,32 @@
 ﻿
 using System.Web.Mvc;
 using van.Core.DataInterfaces;
+using SharpArch.Core;
+using SharpArch.Web;
+using van.Core;
+using System.Collections.Generic;
+
 
 namespace van.Web.Controllers
 {
     public class RecordingsController : Controller
     {
+        private readonly IRecordingRepository _recordingRepository;
+
         public RecordingsController(
-            IRecordingRepository recordingRepository)   {}
+            IRecordingRepository recordingRepository)
+        {
+            Check.Require(recordingRepository != null, "_recordingRepository may not be null");
+
+            this._recordingRepository = recordingRepository;
+        }
 
         public ActionResult ListRecordingsMatching(string filter)
         {
-            return null;
+            List<Recording> matchingRecording = _recordingRepository.FindAllMatching(filter);
+
+            return View("ListRecoringsMatchingFilter", matchingRecording);
+
         }
     }
 }
