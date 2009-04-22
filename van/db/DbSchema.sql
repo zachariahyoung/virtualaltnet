96e166018b53a7c8f04dbdb2fa3b5c2ffa14bcff
JJVLeger <JJVLeger@50af15be-2897-11de-ae70-5144d73449a4>
2009-04-22 03:19:50 +0000

    Adding DBSchema and NUnit 2.4.8

---
+++ van/db/DbSchema.sql
@@ -0,0 +1,9 @@
+if exists (select * from dbo.sysobjects where id = object_id(N'Recordings') and OBJECTPROPERTY(id, N'IsUserTable') = 1) drop table Recordings
+create table Recordings (
+  Id INT IDENTITY NOT NULL,
+   RecordingDuration NVARCHAR(255) null,
+   RecordingTitle NVARCHAR(255) null,
+   RecordingUrl NVARCHAR(255) null,
+   RecordingDate DATETIME null,
+   primary key (Id)
+)
