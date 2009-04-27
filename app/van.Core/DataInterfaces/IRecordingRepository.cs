a97df2640c5e26efd3d749cb76a54354df31a1e7
JJVLeger <JJVLeger@50af15be-2897-11de-ae70-5144d73449a4>
2009-04-27 01:52:42 +0000

    Removed repository extras which are not needed at this point

--- app/van.Core/DataInterfaces/IRecordingRepository.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using SharpArch.Core.PersistenceSupport;
-
-namespace van.Core.DataInterfaces
-{
-    public interface IRecordingRepository : IRepository<Recording> {
-        IList<Recording> FindAllMatching(string filter);
-    }
-}
