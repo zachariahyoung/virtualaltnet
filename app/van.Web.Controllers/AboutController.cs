c5f93f0f3bfd227ba400e09c24572b858ada5b09
zpyoung <zpyoung@50af15be-2897-11de-ae70-5144d73449a4>
2009-05-03 03:14:25 +0000

    Added controller for about page

---
+++ app/van.Web.Controllers/AboutController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace van.Web.Controllers
+{
+    [HandleError]
+    public class AboutController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
