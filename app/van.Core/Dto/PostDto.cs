0d2696d41b17454536e741aaef8fa7d464c4f746
zpyoung <zpyoung@50af15be-2897-11de-ae70-5144d73449a4>
2009-07-30 02:39:32 +0000

    Add DTO

---
+++ app/van.Core/Dto/PostDto.cs
@@ -0,0 +1,10 @@
+namespace van.Core.Dto
+{
+    public class PostDto
+    {
+        public string Url { get; set; }
+        public string Title { get; set; }
+        public string Date { get; set; }
+        public string Content { get; set; } 
+    }
+}
